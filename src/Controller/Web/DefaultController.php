915f5c5094c23e3fba3150caca2aeea7ffcce8f0
Олег Волошин <vololeg93@gmail.com>
2019-01-29 17:50:45 +0200

    created template for default controller

--- src/Controller/Web/DefaultController.php
+++ src/Controller/Web/DefaultController.php
@@ -2,21 +2,19 @@
 
 namespace App\Controller\Web;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
-
-        return new Response(
-            '<html><body>Home</body></html>'
-        );
+        return $this->render('default/index.html.twig', []);
     }
 
 }
